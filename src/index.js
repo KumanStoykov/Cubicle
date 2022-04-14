00c0fd1be3295b5e05054b4803ed60619dd2fc9a
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-14 20:31:45 +0200

    Add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+const hbs = handlebars.create({
+    extname: 'hbs',
+});
+
+app.set('views', path.resolve('./src/views'));
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
 app.all('/', (req, res) => {
     
-    res.send('It\' working');
+    res.render('index', { layout: false});
 });
 
 app.listen(3000, console.log.bind(console, 'Application is running on http://localhost:3000'));
