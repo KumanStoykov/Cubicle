f409ceeeb73e1f0873d2755bc2d9c99070b30364
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-14 20:44:50 +0200

    Add layot

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ const path = require('path');
 
 const app = express();
 
+app.set('views', path.resolve('./src/views'));
+
 const hbs = handlebars.create({
     extname: 'hbs',
 });
-
-app.set('views', path.resolve('./src/views'));
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
 app.all('/', (req, res) => {
     
-    res.render('index', { layout: false});
+    res.render('index');
 });
 
 app.listen(3000, console.log.bind(console, 'Application is running on http://localhost:3000'));
