e552ffb47e6df3d415070a78d879488095ac114b
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-20 21:02:55 +0200

    Add create accessory action

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const router = require('express').Router();
 
-const router = express.Router();
+const accessoryService = require('../services/accessoryService');
 
 router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
-router.post('/create', (req, res) => {
-    let accessory = req.body;
-    console.log(accessory);
+router.post('/create', async (req, res) => {
+    let { name, description, imageUrl } = req.body;
+
+    await accessoryService.create(name, description, imageUrl);
 
     res.redirect('/');
 })
