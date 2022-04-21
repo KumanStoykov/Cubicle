f20f690942f6cac926f2b4664f9d63607f649122
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-21 12:05:31 +0200

    Add getAll function

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -4,8 +4,13 @@ async function create(name, description, imageUrl) {
    return Accessory.create({ name, description, imageUrl });
 }
 
+async function getAll() {
+    return Accessory.find({}).lean();
+}
+
 const accessoryService = {
     create,
+    getAll,
 }
 
 module.exports = accessoryService;
