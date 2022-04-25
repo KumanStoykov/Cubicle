868fee4afcbd8eba71057aab4b240bd2755a019a
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-25 21:54:27 +0200

    Create cube auth middleware

---
+++ src/middlewares/cubeAuthMiddleware.js
@@ -0,0 +1,16 @@
+const cubeService = require('../services/cubeServices');
+
+const isOwnerCube = async (req, res, next) => {
+    let cube = await cubeService.getOne(req.params.cubeId);
+    
+
+    if (cube.owner == req.user._id) {
+        req.cube = cube;
+
+        next();
+    } else {
+        next('You are not authorized to edit this cube');
+    }
+}; 
+
+module.exports = isOwnerCube;
