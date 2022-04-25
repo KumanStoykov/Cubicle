93704d932acef897a3d5184061652bc4ac26abce
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-25 11:52:29 +0200

    Add authorization service

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 
 router.get('/login', (req, res) => {
-   res.render('auth/login'); 
+    res.render('auth/login');
 });
 
 router.post('/login', (req, res) => {
-    
+
     res.redirect('/');
 });
 
 router.get('/register', (req, res) => {
-   res.render('auth/register'); 
+    res.render('auth/register');
 });
 
 router.post('/register', (req, res) => {
-   res.redirect('/'); 
+    let { username, password, repeatPassword } = req.body;
+
+    authService.register(username, password);
+        res.redirect('/');
 });
 
 
---
+++ src/services/authService.js
@@ -0,0 +1,9 @@
+const register = (username, password) => {
+    console.log(username, password);
+};
+
+const authService = {
+    register,
+};
+
+module.exports = authService;
