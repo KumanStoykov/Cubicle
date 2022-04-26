0f591cd3ce65e421e0435e78cf7be42f31660780
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-26 19:39:13 +0200

    Remove status code

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -25,7 +25,7 @@ function auth(req, res, next) {
 function isAuth(req, res, next)  {
 
     if(!req.user) {
-        return res.status(401).redirect('/login');
+        return res.redirect('/login');
     }
     next();
 }
